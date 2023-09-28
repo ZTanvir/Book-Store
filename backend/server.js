e428163dbdfa059b09bbae215362337ee0a99983
ztanvir <zahirulopel@gmail.com>
2023-09-28 11:06:12 +0600

    Add an error handling middle

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.get("/api/books", (request, response) => {
 });
 
 // add new book
-app.post("/api/books", (request, response) => {
+app.post("/api/books", (request, response, next) => {
   const body = request.body;
   if (!body.title || !body.author || !body.publishYear) {
     return response.status(404).json({
@@ -32,19 +32,39 @@ app.post("/api/books", (request, response) => {
     publishYear: body.publishYear,
   });
   // save book to mongodb
-  book.save().then((saveBook) => {
-    response.json(saveBook);
-  });
+  book
+    .save()
+    .then((saveBook) => {
+      response.json(saveBook);
+    })
+    .catch((error) => next(error));
 });
 
 // get a single book
-app.get("/api/books/:id", (request, response) => {
+app.get("/api/books/:id", (request, response, next) => {
   const id = request.params.id;
-  Book.findById(id).then((book) => {
-    response.json(book);
-  });
+  Book.findById(id)
+    .then((book) => {
+      if (book) {
+        response.json(book);
+      } else {
+        response.status(404).send({ error: "Book id not found" });
+      }
+    })
+    .catch((error) => next(error));
 });
 
+// handle all the error related to route
+const handleRouteError = (error, request, response, next) => {
+  console.log("Error name", error.name);
+  if (error.name === "CastError") {
+    response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(handleRouteError);
 app.listen(PORT || 3001, () => {
   console.log(`Server start on port ${PORT}`);
 });
